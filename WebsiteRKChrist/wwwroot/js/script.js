a7e2d1fe288959d132d5115bca34b0617fe2d677
RKChrist <roo12ney@gmail.com>
2022-02-24 13:38:02 +0100

    Changed from english to danish, in the future it should run in both lanugages

--- WebsiteRKChrist/wwwroot/js/script.js
+++ WebsiteRKChrist/wwwroot/js/script.js
@@ -63,14 +63,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Firefighter", "Freelancer"],
+        strings: ["BackEnd Developer", "Uddannet Brandmand", "Studerende"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Firefighter", "Freelancer"],
+        strings: ["BackEnd Developer", "Uddannet Brandmand", "Studerende"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
