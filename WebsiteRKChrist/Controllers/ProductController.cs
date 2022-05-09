a8eabfab87210d68d723edb643fdf488c90b62e6
RKChrist <roo12ney@gmail.com>
2022-05-09 21:21:11 +0200

    Product

---
+++ WebsiteRKChrist/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebsiteRKChrist.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ProductController()
+        {
+            
+        }
+        // GET: ProjectController
+        public ActionResult Index()
+        {
+            ViewBag.Product = "Shown";
+            return View();
+        }
+
+        // GET: ProjectController/Details/5
+        public ActionResult Details(int id)
+        {
+            ViewBag.Product = "Shown";
+            return View();
+        }
+
+        // GET: ProjectController/Create
+        public ActionResult Create()
+        {
+            ViewBag.Product = "Shown";
+            return View();
+        }
+
+       
+    }
+}
